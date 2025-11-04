5dfa87ab79b21b8dfca04a02bcf4245cd08a9a39
SimonBui <simonbui1999@gmail.com>
2025-11-04 20:51:48 +1000

    first commite

---
+++ NoteApp/Controllers/NoteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NoteApp.Models;
+
+namespace NoteApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly List<Note> _notes;
+
+        public NoteController() {
+            _notes = new List<Note>()
+            {
+                new Note() { Title = "firs Note", Content = "first Content" },
+                new Note() { Title = "Second Title", Content = " Second Content" }
+            };
+            _notes.Add(new Note() { Title = "Third Title", Content = "Third Content" });
+        
+        }
+         
+        [HttpGet("/GetAll")]
+        public ActionResult<List<Note>> GetAll()
+        {
+
+            return Ok(this._notes);
+
+        }
+    }
+}
---
+++ NoteApp/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace NoteApp.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateAt { get; set; }
+
+    }
+}
